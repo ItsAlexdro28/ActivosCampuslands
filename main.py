ecad736c18cdf421588993fc2373f3f4753b4dea
ItsAlexdro28 <alejandroj5025@gmail.com>
2024-02-28 11:56:17 -0500

    funcion principal definida

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print('aaaaaa')
-print('eeeeeeeeeeeeeo0000')
+import modules.menus as menu
+import modules.import as imp
+data = {}
+
+if __name__ == "__main__":
+    data = imp.readJson('data')
+    menu.menuprincipal()
