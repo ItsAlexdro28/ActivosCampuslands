e9d11105683f5d4b187bc85c88d22307f55b1e7e
ItsAlexdro28 <alejandroj5025@gmail.com>
2024-02-27 22:30:28 -0500

    agregar los datos

--- modules/import.py
+++ modules/import.py
@@ -1,7 +1,18 @@
-from csv import reader
-data = []
-with open('', 'r') as file:
-    lector = reader(file)
-    for row in lector:
-        data.append(row[0].split(';'))
-print(data)
+import csv
+import json
+data = {}
+with open('data/data.csv', 'r') as file:
+    reader = csv.reader(file)
+    headers = next(reader)
+    key_column_index = 2
+    for row in reader:
+        key = row[key_column_index]
+        row_dict = {}
+        for i, value in enumerate(row):
+            row_dict[headers[i]] = value
+        data[key] = row_dict
+
+with open('data/data.json', 'w') as outfile:
+    json.dump(data, outfile, indent=4)  
+
+print(f"Data saved to data/data.json")
