c57b9ca73883c560d067a1c5b8ba1fef0d196304
Lauraw100 <lauvanetorres1223@gmail.com>
2024-02-29 06:55:09 -0500

    aguegar personal y zonas

--- modules/add.py
+++ modules/add.py
@@ -1,3 +1,6 @@
+from tabulate import tabulate
+import os
+
 def activosAdd(data:dict):
     try:
         new = {}
@@ -27,5 +30,43 @@ def activosAdd(data:dict):
 # cuando termine el "for" se agregaran los datos a el diccionario 'new' con la llave del codigo campus para identificarlo
 # cuando pregunte por 'marca, categoria y tipo' imprimira valores sugeridos segun los requerimientos del cliente
 # el historial no se pregunta porque no hay record del activo hasta ahora, y se añade la lista donde luego estaran las identificaciones de activo
+def addpeople(data:dict):
+    os.system('cls')
+    hold2={}
+    titulo=[["AÑADIR PERSONAS"]]
+    print(tabulate(titulo,tablefmt="double_grid"))
+    id=input('Ingrese id:\n').upper()
+    nombre=input('Ingrese un nombre:\n').upper()
+    email=input('Ingrese un email:\n').upper()
+    telefono=input('Ingrese un telfono:\n').upper()
+    celular=input('Ingrese un celular:\n').upper()
+    people={
+        "Nombre":nombre,
+        "Id":id,
+        "Email":email,
+        "Telefono":telefono,
+        "Celular":celular
+    }
+    hold2[people[id]]=people
+    data['Personal'].update(hold2)
+
+def addzone(data:dict):
+    os.system('cls')
+    hold3={}
+    titulo=[["AÑADIR ZONAS"]]
+    print(tabulate(titulo,tablefmt="double_grid"))
+    nrozona=input('Ingrese el nro zona:\n').upper()
+    nombrezona=input('Ingrese el nombre de la zona:\n').upper()
+    totalcapacidad=input('Ingrese la capacidad de la zona\n').upper()
+    zon={
+        "NroZona":nrozona,
+        "NombreZona":nombrezona,
+        "totalCapacidad":totalcapacidad
+    }
+    hold3[zon[nombrezona]]=zon
+    data['Zonas'].update(hold3)
+
+
+    
 
     
