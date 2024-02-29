283d0844c67ca94371e3d401c82fd93622e74bfe
Lauraw100 <lauvanetorres1223@gmail.com>
2024-02-29 14:59:03 -0500

    edits zonas y personal

--- modules/edit.py
+++ modules/edit.py
@@ -1,6 +1,6 @@
 from tabulate import tabulate
 def activosEdit(data:dict):
-    titulo=[["AÑADIR ACTIVOS"]]
+    titulo=[["EDITAR ACTIVOS"]]
     print(tabulate(titulo,tablefmt="double_grid"))
     codCampus = input("Ingresa el valor del Codigo de campus para editar")
     #LISTA DE LLAVES PARA QUE EL USUARIO LO LEA MAS BONITO
@@ -24,7 +24,7 @@ def activosEdit(data:dict):
                         newValue = input(f"Ingresa el nuevo valor para '{keysRead[x]}': ")
                         activeToEdit[key] = newValue
                     x += 1
-                print(f"Activo '{codCampus}' Editado satisfactoriamente.")
+                print(f"Activo '{codCampus}' fue editado satisfactoriamente.")
                 return
         if indexToEdit == -1:
             print("Cancelado")
@@ -51,3 +51,90 @@ def activosEdit(data:dict):
 # Luego, la función actualiza los detalles del activo en el diccionario de datos.
 # Finalmente muestra un mensaje indicando si el activo se actualizó exitosamente o no.
 
+def peopleEdit(data:dict):
+    titulo=[["EDITAR PERSONAL"]]
+    print(tabulate(titulo,tablefmt="double_grid"))
+    id = input("Ingresa el id de la persona para editar")
+    #LISTA DE LLAVES PARA QUE EL USUARIO LO LEA MAS BONITO
+    info = ['Nombre','Id','Email','Telefono','Celular']
+    if id in data['Personal']:
+        peopleToEdit = data['Personal'][id]
+        print("Detalles del personal:")
+        for i, (key, value) in enumerate(peopleToEdit.items()):
+            if key != 'Historial':  # Saltar el historial
+                print(f"{i+1}. {info[i]}: {value}")
+        try:
+            indexToEdit = int(input("Ingrese el numero de identificacion del valor que desea editar (-1 para cancelar o 0 para editarlos todos): "))
+        except ValueError:
+            print("Invalid input. Escriba un entero.")
+            return #VOLVER AL MENU PRINCIPAL
+        if indexToEdit == 0:
+                # Edit todos los valores
+                for key, value in peopleToEdit.items():
+                    x = 0
+                    if key != 'Historial':  # Saltar el historial
+                        newValue = input(f"Ingresa el nuevo valor para '{info[x]}': ")
+                        peopleToEdit[key] = newValue
+                    x += 1
+                print(f"El personal con id #'{id}' fue editado satisfactoriamente.")
+                return
+        if indexToEdit == -1:
+            print("Cancelado")
+            return
+
+        if 0 < indexToEdit < len(list(peopleToEdit.items())):
+            keyToEdit, _ = list(peopleToEdit.items())[indexToEdit-1]  # Extraer llave
+            keyToPrint, _ = list(info[indexToEdit-1])
+            newValue = input(f"Ingresa el nuevo valor para '{keyToPrint}': ")
+            peopleToEdit[keyToEdit] = newValue
+            print(f"Activo '{peopleToEdit}' Actualizado.")
+            return
+        else:
+            print(f"Index invalido: {indexToEdit}. Ingresa un valor valido entre -1 y {len(indexToEdit) - 1}.")
+          
+    else:
+        print(f"El personal con id#{id} '{indexToEdit}' no ha sido encontrado.")
+
+def zonaEdit(data:dict):
+    titulo=[["EDITAR ZONAS"]]
+    print(tabulate(titulo,tablefmt="double_grid"))
+    nombre= input("Ingrese el nombre de la zona para editar")
+    #LISTA DE LLAVES PARA QUE EL USUARIO LO LEA MAS BONITO
+    info2 = ['NroZona','NombreZona','totalCapacidad']
+    if nombre in data['Zonas']:      
+        zoneToEdit = data['Zonas'][nombre]
+        print("Detalles de la zona:")
+        for i, (key, value) in enumerate(zoneToEdit.items()):
+            if key != 'Historial':  # Saltar el historial
+                print(f"{i+1}. {info2[i]}: {value}")
+        try:
+            indexToEdit = int(input("Ingrese el numero de identificacion del valor que desea editar (-1 para cancelar o 0 para editarlos todos): "))
+        except ValueError:
+            print("Invalid input. Escriba un entero.")
+            return #VOLVER AL MENU PRINCIPAL
+        if indexToEdit == 0:
+                # Edit todos los valores
+                for key, value in zoneToEdit.items():
+                    x = 0
+                    if key != 'Historial':  # Saltar el historial
+                        newValue = input(f"Ingresa el nuevo valor para '{info2[x]}': ")
+                        zoneToEdit[key] = newValue
+                    x += 1
+                print(f"La zona '{nombre}' fue editada satisfactoriamente.")
+                return
+        if indexToEdit == -1:
+            print("Cancelado")
+            return
+
+        if 0 < indexToEdit < len(list(zoneToEdit.items())):
+            keyToEdit, _ = list(zoneToEdit.items())[indexToEdit-1]  # Extraer llave
+            keyToPrint, _ = list(info2[indexToEdit-1])
+            newValue = input(f"Ingresa el nuevo valor para '{keyToPrint}': ")
+            zoneToEdit[keyToEdit] = newValue
+            print(f"Zona '{zoneToEdit}' Actualizada.")
+            return
+        else:
+            print(f"Index invalido: {indexToEdit}. Ingresa un valor valido entre -1 y {len(indexToEdit) - 1}.")
+          
+    else:
+        print(f"La zona '{indexToEdit}' no ha sido encontrada.")
